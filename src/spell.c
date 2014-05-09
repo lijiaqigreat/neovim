7a375ce23dbb4656da3938150035f323a83f1e76
Jiaqi <lijiaqigreat@gmail.com>
2014-05-09 16:50:26 -0500

    reverse if data != NULL

--- src/spell.c
+++ src/spell.c
@@ -2354,8 +2354,11 @@ static void slang_clear(slang_T *lp)
   gap = &lp->sl_sal;
   if (lp->sl_sofo) {
     // "ga_len" is set to 1 without adding an item for latin1
+    if(gap->ga_data != NULL) {
     // SOFOFROM and SOFOTO items: free lists of wide characters.
-    ga_clear_strings(gap);
+      ga_clear_strings(gap);
+    }
+    ga_clear(gap);
   } else {
   // SAL items: free salitem_T items
     GA_DEEP_CLEAR(gap,salitem_T, salitem_clear);
